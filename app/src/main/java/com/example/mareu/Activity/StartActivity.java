bd45680c70bdb7ddc6f40ed85cf4eddeec10777a
Sandrine <61731368+elonaenjy@users.noreply.github.com>
2021-03-04 13:25:25 +0100

    mise en place viewModel

--- app/src/main/java/com/example/mareu/Activity/StartActivity.java
+++ app/src/main/java/com/example/mareu/Activity/StartActivity.java
@@ -1,6 +1,7 @@
 package com.example.mareu.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,34 +18,38 @@ import com.example.mareu.R;
 import java.util.List;
 
 public class StartActivity extends AppCompatActivity {
-    public List<Guest> lGuests = MyViewModel.generateGuests();
-    public List<Meeting> lMeetings = MyViewModel.generateMeetings();
-    public List<Room> lRooms = MyViewModel.generateRooms();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_start );
+        MyViewModel model = new ViewModelProvider( this ).get( MyViewModel.class );
+        model.getMeeting().observe( this, lMeetings -> {
+        } );
+        model.getGuest().observe( this, lGuests -> {
+        } );
+        model.getRoom().observe( this, lRooms -> {
+        } );
 
-        ImageView imageView = findViewById(R.id.image_startscreen);
+        ImageView imageView = findViewById( R.id.image_startscreen );
         // Create the animation.
 
-        Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
-        anim.setAnimationListener(new Animation.AnimationListener() {
+        Animation anim = AnimationUtils.loadAnimation( getApplicationContext(), R.anim.fade_in );
+        anim.setAnimationListener( new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                Intent intent = new Intent(StartActivity.this, ListMeetingActivity.class);
-                startActivity(intent);
+                Intent intent = new Intent( StartActivity.this, ListMeetingActivity.class );
+                startActivity( intent );
             }
 
             @Override
             public void onAnimationRepeat(Animation animation) {
             }
-        });
-        imageView.startAnimation(anim);
+        } );
+        imageView.startAnimation( anim );
     }
 }
--- app/src/main/java/com/example/mareu/Model/MyViewModel.java
+++ app/src/main/java/com/example/mareu/Model/MyViewModel.java
@@ -1,31 +1,48 @@
 package com.example.mareu.Model;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.List;
 
 public class MyViewModel extends ViewModel {
-
-    private final List<Meeting> lMeetings = Meeting.generateMeetings();
-    private final List<Guest> lGuests = Guest.generateGuests();
-    private final List<Room> lRooms = Room.generateRooms();
-
-    public List<Meeting> getMeetings() { return lMeetings;   }
-
-    public void deleteMeeting(Meeting meeting) {
-        this.lMeetings.remove(meeting);
+    private MutableLiveData<List<Meeting>> lMeetings;
+    public LiveData<List<Meeting>> getMeeting() {
+        if (lMeetings == null) {
+            lMeetings = new MutableLiveData<List<Meeting>>();
+            Meeting.generateMeetings();
+        }
+        return lMeetings;
     }
 
-    public void addMeeting(Meeting meeting) {
-        this.lMeetings.add(meeting);
+    private MutableLiveData<List<Guest>> lGuests;
+    public LiveData<List<Guest>> getGuest() {
+        if (lGuests == null) {
+            lGuests = new MutableLiveData<List<Guest>>();
+            Guest.generateGuests();
+        }
+        return lGuests;
     }
 
-    public List<Guest> getGuests() {
-        return this.lGuests;
+    private MutableLiveData<List<Room>> lRooms;
+    public LiveData<List<Room>> getRoom() {
+        if (lRooms == null) {
+            lRooms = new MutableLiveData<List<Room>>();
+            Room.generateRooms();
+        }
+        return lRooms;
     }
 
-    public List<Room> getRooms() {
-        return this.lRooms;
+
+//    public void deleteMeeting(Meeting meeting) {
+//        this.lMeetings.remove(meeting);
+//    }
+
+//    public void addMeeting(Meeting meeting) {
+//        this.lMeetings.add(meeting);
+//    }
+
     }
 
 
